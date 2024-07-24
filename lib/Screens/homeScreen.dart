c75966830e345a9bec21d26a61a0b9e9e8a6e576
Krish Shrestha <103551556+krishrestha@users.noreply.github.com>
2024-07-24 17:51:46 +0545

    edu app

---
+++ lib/Screens/homeScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatelessWidget {
+  const homeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
